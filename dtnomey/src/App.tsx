91d67ccc7377e610a57a8de28acb022e53994137
lucas.bueno <lucas.bueno@BNU-LUCASB>
2021-02-24 22:03:47 -0300

    init

---
+++ dtnomey/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ExperienceBar } from './Components/ExperienceBar';
+import './styles/global.css';
+
+function App() {
+  return (
+   <div className='container'>
+     <ExperienceBar/>
+   </div>
+  );
+}
+
+export default App;
